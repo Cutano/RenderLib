94d8a06793f4159213b1ca4873b9d7bc97fea67e
Cutano <rinne@rinne.top>
2022-10-26 11:49:22 +0800

    Refactor submodule

--- GenerateDepsProj.bat
+++ GenerateDepsProj.bat
@@ -1,1 +1,2 @@
-cmake .\ThirdParty\spdlog\CMakeLists.txt -B .\ThirdParty\spdlog\build
+cmake -S .\ThirdParty\spdlog -B .\ThirdParty\spdlog\build -A x64
+cmake -D DILIGENT_LOAD_PIX_EVENT_RUNTIME=TRUE -S .\ThirdParty\DiligentEngine -B .\ThirdParty\DiligentEngine\build\Win64 -A x64
