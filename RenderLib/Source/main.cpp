11cf8aa6a12009a2a1fa6495fe97e48901bf0751
Cutano <rinne@rinne.top>
2022-10-26 15:57:27 +0800

    Configure project deps

---
+++ RenderLib/Source/main.cpp
@@ -0,0 +1,12 @@
+extern "C" {
+	__declspec(dllexport) extern constexpr unsigned int D3D12SDKVersion = 606;
+}
+
+extern "C" {
+	__declspec(dllexport) extern const char8_t* D3D12SDKPath = u8".\\D3D12\\";
+}
+
+int main()
+{
+	return 0;
+}
