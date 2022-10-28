7b3c5520cdebaf1a76fdd9015b42627f94efa832
Cutano <rinne@rinne.top>
2022-10-28 11:41:12 +0800

    Use DeveloperPaul123/eventbus submodule instead

--- GenerateDepsProj.bat
+++ GenerateDepsProj.bat
@@ -1,3 +1,2 @@
 cmake -S .\ThirdParty\spdlog -B .\ThirdParty\spdlog\build -A x64
-cmake -D ENABLE_TEST=FALSE -S .\ThirdParty\EventBus -B .\ThirdParty\EventBus\build -A x64
 cmake -D DILIGENT_LOAD_PIX_EVENT_RUNTIME=TRUE -S .\ThirdParty\DiligentEngine -B .\ThirdParty\DiligentEngine\build\Win64 -A x64
